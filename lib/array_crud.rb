1a6ac4f4fa1abc421f913d2b5faa4e78f66f72eb
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-24 21:44:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,37 +3,37 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [one, two, three, four]
+  ['one', 'two', 'three', 'four']
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << element 
+  array << element 
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift(element) 
+  array.unshift(element) 
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop(element)
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element 
 end
