8957c16f549f36eff371e0565a19bf1827acec56
Curtis Arnold <arnold.curtis@gmail.com>
2019-06-24 21:36:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,19 +3,19 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  [one, two, three, four]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << element 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift(element) 
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
